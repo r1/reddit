2107d0d5d81b6a1f590ec2b23c7e3f869db28f13
Jack Latimer <jacklatimer03@gmail.com>
2021-07-05 22:40:44 +0100

    done(file structure): changed where the UI goes

--- components/ui/NavBar/SearchBar.tsx
+++ components/ui/NavBar/SearchBar.tsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
 const Search = styled.input`
+  outline: none;
+  color: #242424;
   background-color: #161616;
   font-weight: bold;
   width: 19em;
   height: 3.6em;
   border-radius: 55px;
-  border: 1px solid gray;
+  border: 1px solid #282828;
   padding: 0 16px 0 24px;
 `;
 
